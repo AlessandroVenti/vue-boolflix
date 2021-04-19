0a8012ae667de54601d03e05eae1b47ae3bc1739
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-19 13:01:23 +0200

    Working on v-for

--- js/script.js
+++ js/script.js
@@ -10,33 +10,39 @@
 
 function createVueInstance() {
   new Vue({
-      el: '#jsMusic',
+      el: '#jsBoolflix',
 
       data: {
-          array : []
+          movieArray : [],
+          searchInput : ``
       },
 
       mounted() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-          params: {
-            'api_key' : 'ae4586143be06066dd74693192e929fc',
-            'query' : 'batman'
-          }
-        })
-        .then(data => {
+        
+      },
 
-          console.log(data.data.results);
-        })
+      computed: {
 
-        .catch(() => console.log('error'));
       },
 
       methods: {
 
-        lollipop: function() {
-          return this.data.data.results;          
+        searchMovie: function() {
+          axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+              'api_key' : 'ae4586143be06066dd74693192e929fc',
+              'query' : this.searchInput
+            }
+          })
+          .then(data => {
+  
+            this.movieArray = data.data.results;
+            console.log(this.movieArray);
+            return this.movieArray;
+          })
+  
+          .catch(() => console.log('error'));
         }
-
       }
   });
 }

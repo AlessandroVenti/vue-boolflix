e23313b26769e6099de28869e503119ab63df9cd
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-19 17:34:02 +0200

    third milestone done

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@
 // crubs
 
 
+// Milestone 3:
+// In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. 
+// Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. 
+// Dovremo prendere quindi l’URL base delle immagini 
+// di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare 
+// (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) 
+// per poi aggiungere la parte finale dell’URL passata dall’API.
+// Esempio di URL:
+// https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+
+
 function createVueInstance() {
   new Vue({
       el: '#jsBoolflix',
@@ -22,7 +33,8 @@ function createVueInstance() {
       data: {
           movieArray : [],
           tvSeriesArray : [],
-          searchInput : ``
+          searchInput : ``,
+          imgInput: `https://image.tmdb.org/t/p/w92`
       },
 
       mounted() {
@@ -44,11 +56,11 @@ function createVueInstance() {
           })
           .then(data => {
               this.movieArray = data.data.results;
-            console.log(this.movieArray);
-            return this.movieArray;
+              console.log(this.movieArray);
           })
           .catch(() => console.log('error'));
         },
+        
 
         searchTvSeries: function() {
           axios.get('https://api.themoviedb.org/3/search/tv', {
@@ -60,12 +72,16 @@ function createVueInstance() {
           .then(data => {
               this.tvSeriesArray = data.data.results;
             console.log(this.tvSeriesArray);
-            return this.seriesArray;
           })
           .catch(() => console.log('error'));
-        }
-      }
+        },
 
+
+        vote1To5: function(vote) {
+         return vote = Math.round(vote / 2);
+        }
+
+      }
   });
 }
 

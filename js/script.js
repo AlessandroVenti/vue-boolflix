9b46d12a45850acca7ff8af24937535f9d75fca7
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-19 14:52:24 +0200

    second milestone done

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@
 // 3. Lingua
 // 4. Voto
 
+// Milestone 2:
+// Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
+// crubs
+
 
 function createVueInstance() {
   new Vue({
@@ -14,6 +21,7 @@ function createVueInstance() {
 
       data: {
           movieArray : [],
+          tvSeriesArray : [],
           searchInput : ``
       },
 
@@ -22,7 +30,7 @@ function createVueInstance() {
       },
 
       computed: {
-
+        
       },
 
       methods: {
@@ -35,15 +43,29 @@ function createVueInstance() {
             }
           })
           .then(data => {
-  
-            this.movieArray = data.data.results;
+              this.movieArray = data.data.results;
             console.log(this.movieArray);
             return this.movieArray;
           })
-  
+          .catch(() => console.log('error'));
+        },
+
+        searchTvSeries: function() {
+          axios.get('https://api.themoviedb.org/3/search/tv', {
+            params: {
+              'api_key' : 'ae4586143be06066dd74693192e929fc',
+              'query' : this.searchInput
+            }
+          })
+          .then(data => {
+              this.tvSeriesArray = data.data.results;
+            console.log(this.tvSeriesArray);
+            return this.seriesArray;
+          })
           .catch(() => console.log('error'));
         }
       }
+
   });
 }
 
